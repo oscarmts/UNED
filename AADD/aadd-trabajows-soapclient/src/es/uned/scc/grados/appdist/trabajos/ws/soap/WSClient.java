3272f43cfb02f7e521e54069c515bfcd3f62124c
oscarmts <16204530+oscarmts@users.noreply.github.com>
2022-03-09 10:51:28 +0100

    AADD trabajo ws soapclient

---
+++ AADD/aadd-trabajows-soapclient/src/es/uned/scc/grados/appdist/trabajos/ws/soap/WSClient.java
@@ -0,0 +1,25 @@
+package es.uned.scc.grados.appdist.trabajos.ws.soap;
+
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+import es.uned.scc.grados.appdist.trabajos.ws.OperationInfo;
+import es.uned.scc.grados.appdist.trabajos.ws.common.SignalGeneratorWSHelper;
+import es.uned.scc.grados.appdist.trabajos.ws.frame.PlottingFrame;
+
+public class WSClient {
+
+	public static void main(String[] args) throws ServiceException, RemoteException {
+		SignalGeneratorWSHelper wsHelper = new SignalGeneratorWSHelper();
+		OperationInfo info = wsHelper.getOperationInfo();
+		if (info.isOk()) {
+			System.out.print("Running the signal generator");
+			PlottingFrame window = new PlottingFrame(wsHelper);
+			window.show();
+		} else {
+			System.out.print("Not running: " + info.getMessage());
+		}
+	}
+
+}
